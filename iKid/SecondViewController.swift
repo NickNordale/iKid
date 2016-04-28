12175506667e34bcb93e454b15a18a546ee9b678
Nick-Nordale <nicknordale@gmail.com>
2016-04-28 12:28:38 -0700

    Initial Commit

---
+++ iKid/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iKid
+//
+//  Created by Nick Nordale on 4/28/16.
+//  Copyright © 2016 Nick Nordale. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
